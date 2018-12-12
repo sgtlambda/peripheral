144693d26728c6015122a00becb6ff7a5a283e66
JM Versteeg <janmerijnversteeg@gmail.com>
2018-12-12 17:57:52 +0000

    remove unused aiming logic

--- src/game.js
+++ src/game.js
@@ -1,6 +1,6 @@
 window.decomp = require('poly-decomp');
 
-import {Engine, Runner, Body, Events, Vector} from 'matter-js';
+import {Engine, Runner, Events} from 'matter-js';
 
 import Render from '../fork/renderer';
 import createRenderer from './createRenderer';
@@ -17,24 +17,10 @@ if (canvas) canvas.remove();
 if (window.lastStop) window.lastStop();
 
 
-const ingameMousePos = (mouse, camera) => ({
-    x: mouse.x + camera.panX,
-    y: mouse.y,
-});
-
-const updateAimAngle = ({ctx, player, camera, mouse}) => {
-    const mountPos = player.weaponMount.position;
-    const mousePos = ingameMousePos(mouse, camera);
-    const aimAngle = Vector.angle(mountPos, mousePos);
-    // ctx.translate(mountPos.x - camera.panX, mountPos.y);
-    // ctx.rotate(aimAngle);
-    // ctx.fillStyle   = 'transparent';
-    // ctx.strokeStyle = 'white';
-    // ctx.strokeWidth = '.5px';
-    // ctx.strokeRect(0, -5, 40, 10);
-    // ctx.setTransform(2, 0, 0, 2, 0, 0);
-    return aimAngle;
-};
+// const ingameMousePos = (mouse, camera) => ({
+//     x: mouse.x + camera.panX,
+//     y: mouse.y,
+// });
 
 (() => {
 
@@ -67,30 +53,6 @@ const updateAimAngle = ({ctx, player, camera, mouse}) => {
     Render.run(renderer);
 
     Runner.run(Runner.create(), engine);
-
-    // let mouse = {x: 0, y: 0};
-    //
-    // window.addEventListener('mousemove', e => {
-    //     mouse.x = e.pageX;
-    //     mouse.y = e.pageY;
-    // });
-
-    // let aimAngle;
-
-    // Events.on(renderer, 'afterRender', () => {
-    //     const ctx = renderer.context;
-    //     ctx.beginPath();
-    //     ctx.arc(mouse.x, mouse.y, 5, 0, Math.PI * 2);
-    //     ctx.stroke();
-    //     ctx.fillStyle = 'red';
-
-        // aimAngle = updateAimAngle({player, camera, mouse, ctx});
-    // });
-
-    // window.addEventListener('mousedown', e => {
-    //     const vector = Vector.rotate({x: -.05, y: 0}, aimAngle);
-    //     Body.applyForce(player.collider, {x: 0, y: 0}, vector);
-    // });
 
     window.lastStop = () => {
         Render.stop(renderer);
