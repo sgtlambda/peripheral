da0d7634200c93dc57397296f510865a11481511
Jan-Merijn Versteeg <janmerijnversteg@gmail.com>
2022-11-11 00:28:40 +0100

    ts-ify interactionController

---
+++ src/interactionController.ts
@@ -0,0 +1,52 @@
+export default (
+  {
+    mouseEmitter = window,
+    keyEmitter = document,
+    interactionHandler,
+    keyMap = {
+      // The values here map to methods in the InteractionHandler class
+      q: 'dropItem',
+      b: 'buildItem',
+      e: 'takeItem',
+      t: 'throwItem',
+    },
+  }: {
+    mouseEmitter?: EventTarget;
+    keyEmitter?: EventTarget;
+    interactionHandler: any; // TODO ts-ify `InteractionHandler`
+    keyMap?: Record<string, string>;
+  }) => {
+
+  const keysOn = {};
+
+  const press = e => {
+    const method = keyMap[e.key];
+    if (method && interactionHandler[method])
+      interactionHandler[method].call(interactionHandler);
+  };
+
+  let _triggerContinuous;
+
+  const mouseDown = () => {
+    interactionHandler.triggerPrimary();
+    _triggerContinuous = setInterval(() => interactionHandler.triggerContinuous(), 200);
+  };
+
+  const mouseUp = () => {
+    clearInterval(_triggerContinuous);
+  };
+
+  keyEmitter.addEventListener('keydown', press);
+
+  mouseEmitter.addEventListener('mousedown', mouseDown);
+  mouseEmitter.addEventListener('mouseup', mouseUp);
+
+  return {
+    keysOn,
+    destroy() {
+      keyEmitter.removeEventListener('keydown', press);
+      mouseEmitter.removeEventListener('mousedown', mouseDown);
+      mouseEmitter.removeEventListener('mouseup', mouseUp);
+    },
+  };
+};
