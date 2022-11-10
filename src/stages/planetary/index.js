2d71c6acee63fa1e62f896320e5796a825aca3da
Jan-Merijn Versteeg <janmerijnversteg@gmail.com>
2022-11-10 23:48:52 +0100

    larger base planet, more boxes

--- src/stages/planetary/index.js
+++ src/stages/planetary/index.js
@@ -1,39 +1,43 @@
 import {Bodies} from 'matter-js';
+import {range} from "lodash";
 
 import Stage from '../../logic/Stage';
-
 import Planet from '../../logic/Planet';
 
 export default () => {
 
-    const radius = 1000;
+    const radius = 5000;
 
     const stage = new Stage({
-        initialPlayerPos: {x: 0, y: -radius - 100},
+        initialPlayerPos: {x: 400, y: -radius - 100},
     });
 
     stage.addPlanet(Planet.createCircular({
         name:       'moon',
         radius,
-        resolution: 120,
-        rand:       .02,
+        resolution: 50,
+        rand:       0,
     }));
 
     const boxes = [];
 
     // Add a bunch of boxes...
-    const boxSize = 24;
-    [
-        [-5, 2], [-4, 2],
-        [-5, 3], [-4, 3],
-    ].forEach(([bx, by]) => {
-        boxes.push([bx * boxSize,
-            -radius - 238.4820728 + by * boxSize]);
+    const boxSize = 30;
+
+    const boxMatrix = range(-2, 2).flatMap(x => {
+        return range(-2, 0).map(y => [x, y]);
     });
 
+    for (const [bx, by] of boxMatrix) {
+        boxes.push([
+            bx * boxSize,
+            -radius + by * boxSize,
+        ]);
+    }
+
     boxes.forEach(([x, y]) => {
-        stage.addTerrainBody(Bodies.rectangle(x, y, boxSize, boxSize, {
-            render: {fillStyle: '#b2b2b0'}
+        stage.addTerrainBody(Bodies.rectangle(x, y, boxSize*.9, boxSize*.9, {
+            render: {fillStyle: '#6171a2'},
         }));
     });
 
