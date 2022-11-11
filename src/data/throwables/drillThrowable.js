dba050ec6f4437f2ae4ca432ad481d86d832a5ad
Jan-Merijn Versteeg <janmerijnversteg@gmail.com>
2022-11-11 01:37:07 +0100

    drill attributes

--- src/data/throwables/drillThrowable.js
+++ src/data/throwables/drillThrowable.js
@@ -6,8 +6,6 @@ export default ({x, y, velocity}) => new Throwable({
     name: 'drill', x, y, velocity, countdown: 0,
     trigger({position, interactionHandler}) {
         applyExplosion({
-            resolution: 6,
-            rand:       .2,
             stage:      interactionHandler.stage,
             radius:     30,
             force:      0,
