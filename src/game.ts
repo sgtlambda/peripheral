129e80de8caf6e58c2442127248112ea0ee37cc0
Jan-Merijn Versteeg <janmerijnversteg@gmail.com>
2022-11-11 02:29:14 +0100

    move inventory order

--- src/game.ts
+++ src/game.ts
@@ -55,11 +55,11 @@ if ('lastStop' in window) window.lastStop();
 
     const playerState = new PlayerState();
 
+    playerState.addToInventory({itemType: laser});
     playerState.addToInventory({itemType: grenade, amount: 99});
     playerState.addToInventory({itemType: nuke, amount: 99});
     playerState.addToInventory({itemType: drill, amount: 800});
     playerState.addToInventory({itemType: crate, amount: 800});
-    playerState.addToInventory({itemType: laser});
     playerState.addToInventory({itemType: debugDraw, slot: 7});
 
     world.gravity.scale = 0;
