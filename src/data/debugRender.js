9b74fb7182e0cb207a80448c0f203b89713e08fd
Jan-Merijn Versteeg <janmerijnversteg@gmail.com>
2022-12-12 01:02:41 +0100

    debugRender typing

--- src/data/debugRender.js
+++
@@ -1,9 +0,0 @@
-export default {lineWidth: 1, strokeStyle: 'rgba(255,0,0,1)', fillStyle: 'transparent'};
-
-// export default {visible: false};
-
-export const planetDebugRender = {
-    fillStyle:   '#812344',
-    strokeStyle: '#812344',
-    lineWidth:   1,
-};
