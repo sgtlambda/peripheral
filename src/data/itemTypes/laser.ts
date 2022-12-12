90306c7a83cfabe838630eb7db285f2030a30412
Jan-Merijn Versteeg <janmerijnversteg@gmail.com>
2022-12-12 20:53:58 +0100

    more interesting laser

--- src/data/itemTypes/laser.ts
+++ src/data/itemTypes/laser.ts
@@ -3,7 +3,7 @@ import applyIntent from "../intents/applyIntent";
 import {nom} from "../../logic/effects/nom";
 import {Vector} from "matter-js";
 
-export const createLaser = (depth: number = 100, width: number = 1) => new ItemType({
+export const createLaser = (depth: number = 100, width: number = 6, taperTo: number = 0, steps: number = 8, random: number = 30) => new ItemType({
   name:             `laser`,
   color:            '#b8866e',
   availableIntents: [
@@ -13,12 +13,36 @@ export const createLaser = (depth: number = 100, width: number = 1) => new ItemT
       applyActionLabel: 'fire laser',
       apply(player, stage) {
 
-        const shape: Vector[] = [
-          {x: 0, y: width / 2},
-          {x: 0, y: -width / 2},
-          {x: depth, y: -width / 2},
-          {x: depth, y: width / 2},
-        ].map(vector => {
+        const coords = [];
+
+        for (let i = 0; i < steps; i++) {
+          coords.push({
+            x: (depth / steps) * i + (i === 0 ? 0 : Math.random() * random),
+            y: (i === 0 ? 0 : Math.random() * random),
+          });
+        }
+
+        const points: Vector[] = [];
+
+        for (let i = 0; i < steps; i++) {
+          // Calculate the width at this point based on `width` and `taperTo`
+          const widthAtPoint = width - ((width - taperTo) / steps) * i;
+          points.push({
+            x: coords[i].x,
+            y: coords[i].y - widthAtPoint,
+          });
+        }
+
+        // traverse the coords in reverse order
+        for (let i = steps - 1; i >= 0; i--) {
+          const widthAtPoint = width - ((width - taperTo) / steps) * i;
+          points.push({
+            x: coords[i].x,
+            y: coords[i].y + widthAtPoint,
+          });
+        }
+
+        const shape: Vector[] = points.map(vector => {
           return Vector.add(
             {...player.position},
             Vector.rotate(vector, player.aimAngle),
