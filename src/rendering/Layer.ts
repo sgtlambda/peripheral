b8766ca52fa782a01ce958cac8e87661dfea4233
JM Versteeg <5894809+sgtlambda@users.noreply.github.com>
2024-08-09 20:04:05 +0200

    more refactoring

--- src/rendering/Layer.ts
+++ src/rendering/Layer.ts
@@ -4,63 +4,67 @@ import Camera from './Camera';
 
 export type LayerRendererFn = (context: CanvasRenderingContext2D, renderer: Render, camera: Camera) => void;
 
+export type RenderEvent = 'afterRender' | 'beforeRender';
+
 export default class Layer {
 
-    _render: LayerRendererFn;
-    _callback: (e: any) => void;
-    hud: boolean;
-    over: boolean;
-    persistMatrix: boolean;
-    _hook: string;
+  _render: LayerRendererFn;
+  _callback: (e: any) => void;
+  hud: boolean;
+  over: boolean;
+  persistMatrix: boolean;
 
-    /**
-     * @param {boolean} hud Whether the render layer should stay in place (static)
-     * @param {boolean} over Whether this render layer goes over (true) or under (false) the Matter renderer
-     * @param {boolean} persistMatrix Whether to leave the context matrix in place after drawing the layer
-     * @param {function} render The render function
-     */
-    constructor(
-      {
-                    hud = false,
-                    over = true,
-                    persistMatrix = false,
-                    render
-                }: {
+  /**
+   * @param {boolean} hud Whether the render layer should stay in place (static)
+   * @param {boolean} over Whether this render layer goes over (true) or under (false) the Matter renderer
+   * @param {boolean} persistMatrix Whether to leave the context matrix in place after drawing the layer
+   * @param {function} render The render function
+   */
+  constructor(
+    {
+      hud = false,
+      over = true,
+      persistMatrix = false,
+      render
+    }: {
       hud?: boolean;
       over?: boolean;
       persistMatrix?: boolean;
       render: LayerRendererFn;
     }) {
-        this.hud = hud;
-        this.over = over;
-        this.persistMatrix = persistMatrix;
-        this._render = render;
-        this._hook = this.over ? 'afterRender' : 'beforeRender';
+    this.hud           = hud;
+    this.over          = over;
+    this.persistMatrix = persistMatrix;
+    this._render       = render;
+  }
+
+  get event(): RenderEvent {
+    return this.over ? 'afterRender' : 'beforeRender'
+  }
+
+  /**
+   * @param {Render} renderer
+   * @param {Camera} camera
+   */
+  render(renderer: Render, camera: Camera) {
+    const context = renderer.context;
+    if (!this.persistMatrix) context.save();
+    if (!this.hud) {
+      const translate = renderer.bounds.min;
+      camera.rotate(context);
+      context.translate(-translate.x, -translate.y);
     }
 
-    /**
-     * @param {Render} renderer
-     * @param {Camera} camera
-     */
-    render(renderer: Render, camera: Camera) {
-        const context = renderer.context;
-        if (!this.persistMatrix) context.save();
-        if (!this.hud) {
-            const translate = renderer.bounds.min;
-            camera.rotate(context);
-            context.translate(-translate.x, -translate.y);
-        }
+    this._render(context, renderer, camera);
+    if (!this.persistMatrix) context.restore();
+  }
 
-        this._render(context, renderer, camera);
-        if (!this.persistMatrix) context.restore();
-    }
+  attach(renderer: Render, camera: Camera) {
+    this._callback = () => this.render(renderer, camera);
+    Events.on(renderer, this.event, this._callback);
+  }
 
-    attach(renderer: Render, camera: Camera) {
-        this._callback = () => this.render(renderer, camera);
-        Events.on(renderer, this._hook, this._callback);
-    }
-
-    detach(renderer: Render) {
-        Events.off(renderer, this._hook, this._callback);
-    }
+  detach(renderer: Render) {
+    Events.off(renderer, this.event, this._callback);
+  }
 }
