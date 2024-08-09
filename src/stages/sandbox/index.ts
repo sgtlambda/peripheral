97aa42fef668e266718885575f032092d790efff
JM Versteeg <5894809+sgtlambda@users.noreply.github.com>
2024-08-09 20:12:29 +0200

    disable NPCs (for now)

--- src/stages/sandbox/index.ts
+++ src/stages/sandbox/index.ts
@@ -1,6 +1,6 @@
 import Stage from "../../logic/Stage";
 import Planet from "../../logic/Planet";
-import {NPC} from "../../NPC";
+// import {NPC} from "../../NPC";
 
 export default () => {
   const stage = new Stage({x: 0, y: 0});
@@ -20,17 +20,17 @@ export default () => {
     name:     'sandbox-planet',
     color:    '#ffffff',
   }));
-  stage.addNPC(new NPC({
-    id:                   1, name: 'Barry', stage, x: -600, y: 0,
-    additionalNpcContext: "Barry speaks broken German with some English mixed in, but always with a derogatory tone.",
-  }));
-  stage.addNPC(new NPC({
-    id:                   2, name: 'Karel', stage, x: -650, y: 0,
-    additionalNpcContext: "Karel will always accuse the player of looking like a tiny rat, no matter what the player says to try and convince him otherwise."
-  }));
-  stage.addNPC(new NPC({
-    id:                   3, name: 'Gijsbert', stage, x: -700, y: 0,
-    additionalNpcContext: "Gijsbert will always pretend not to understand a single word of English. He'll talk in a made up language."
-  }));
+  // stage.addNPC(new NPC({
+  //   id:                   1, name: 'Barry', stage, x: -600, y: 0,
+  //   additionalNpcContext: "Barry speaks broken German with some English mixed in, but always with a derogatory tone.",
+  // }));
+  // stage.addNPC(new NPC({
+  //   id:                   2, name: 'Karel', stage, x: -650, y: 0,
+  //   additionalNpcContext: "Karel will always accuse the player of looking like a tiny rat, no matter what the player says to try and convince him otherwise."
+  // }));
+  // stage.addNPC(new NPC({
+  //   id:                   3, name: 'Gijsbert', stage, x: -700, y: 0,
+  //   additionalNpcContext: "Gijsbert will always pretend not to understand a single word of English. He'll talk in a made up language."
+  // }));
   return stage;
 };
