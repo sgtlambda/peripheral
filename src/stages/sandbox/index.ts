0488ec76df63619e038bb41dce0c06a4b0e41a5e
JM Versteeg <5894809+sgtlambda@users.noreply.github.com>
2025-01-24 23:42:58 +0100

    NPCs

--- src/stages/sandbox/index.ts
+++ src/stages/sandbox/index.ts
@@ -1,36 +1,32 @@
 import Stage from "../../logic/Stage";
 import Planet from "../../logic/Planet";
-// import {NPC} from "../../NPC";
+import {NPC} from "../../NPC";
 
 export default () => {
   const stage = new Stage({x: 0, y: 0});
   stage.addPlanet(new Planet({
     vertices: [
       {x: -2000, y: 20},
-      {x: 200, y: 20},
-      {x: 200, y: -120},
-      {x: 210, y: -420},
-      {x: 240, y: -420},
-      {x: 250, y: -120},
-      {x: 250, y: 20},
-      {x: 2000, y: 20},
-      {x: 2000, y: 2000},
+      {x: 1200, y: 20},
+      {x: 1200, y: -120},
+      {x: 1210, y: -420},
+      {x: 1240, y: -420},
+      {x: 1250, y: -120},
+      {x: 1250, y: 20},
+      {x: 12000, y: 20},
+      {x: 12000, y: 2000},
       {x: -2000, y: 2000},
     ],
     name:     'sandbox-planet',
     color:    '#ffffff',
   }));
-  // stage.addNPC(new NPC({
-  //   id:                   1, name: 'Barry', stage, x: -600, y: 0,
-  //   additionalNpcContext: "Barry speaks broken German with some English mixed in, but always with a derogatory tone.",
-  // }));
-  // stage.addNPC(new NPC({
-  //   id:                   2, name: 'Karel', stage, x: -650, y: 0,
-  //   additionalNpcContext: "Karel will always accuse the player of looking like a tiny rat, no matter what the player says to try and convince him otherwise."
-  // }));
-  // stage.addNPC(new NPC({
-  //   id:                   3, name: 'Gijsbert', stage, x: -700, y: 0,
-  //   additionalNpcContext: "Gijsbert will always pretend not to understand a single word of English. He'll talk in a made up language."
-  // }));
+  stage.addNPC(new NPC({
+    id:                   1, name: 'Guard', stage, x: 300, y: 0,
+    additionalNpcContext: "Barry speaks broken German with some English mixed in, but always with a derogatory tone.",
+  }));
+  stage.addNPC(new NPC({
+    id:                   2, name: 'Janitor', stage, x: 600, y: 0,
+    additionalNpcContext: "Karel will always accuse the player of looking like a tiny rat, no matter what the player says to try and convince him otherwise."
+  }));
   return stage;
 };
