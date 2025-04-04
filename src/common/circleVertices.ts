9378006a141f09eac5a28c32eacafd1ad410feed
JM Versteeg <5894809+sgtlambda@users.noreply.github.com>
2025-04-04 21:29:57 +0200

    add random angle feature to circleVertices

--- src/common/circleVertices.ts
+++ src/common/circleVertices.ts
@@ -1,12 +1,19 @@
-import {Vector} from "matter-js";
+import {Vector, Vertices} from "matter-js";
 
-export default (radius, resolution, rand = 0): Vector[] => {
+export default (
+  radius: number,
+  resolution: number,
+  radiusRand          = 0,
+  rotateRand: boolean = false,
+): Vector[] => {
   const r = [];
   for (let i = 0; i < resolution; i++) {
-    const distort    = Math.random() * rand * radius - (rand * radius / 2);
+    const distort    = Math.random() * radiusRand * radius - (radiusRand * radius / 2);
     const baseVector = {x: radius + distort, y: 0};
     const angle      = i / resolution * 2 * Math.PI;
     r.push(Vector.rotate(baseVector, angle));
   }
-  return r;
+  return rotateRand
+    ? Vertices.rotate(r, Math.random() * Math.PI * 2, {x: 0, y: 0})
+    : r;
 };
