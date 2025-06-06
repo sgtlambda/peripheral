b6ef5f5baa318088993cf5d76bee931fc156ed62
JM Versteeg <5894809+sgtlambda@users.noreply.github.com>
2025-06-06 23:37:47 +0200

    add more events

--- src/speech-input/bus.ts
+++ src/speech-input/bus.ts
@@ -2,6 +2,8 @@ import mitt from 'mitt';
 
 export type Events = {
     emitWord: (word: string, took: number) => void;
+     activity: () => void;
+    over: () => void;
 };
 
 // TODO should this be a singleton as such or maybe use a context or sth similar?
